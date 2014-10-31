4c4d10e0adea35482a706f925d5c9bc35f9ea650
perpetual <Gary@garyzhao-nb0.local>
2014-10-31 11:21:19 +0800

    加入SampleActivity

---
+++ src/com/example/androiddemo/activity/SampleActivity.java
@@ -0,0 +1,24 @@
+package com.example.androiddemo.activity;
+
+import com.example.androiddemo.R;
+
+/**
+ * <pre>
+ * Copyright (C) 1998-2014 TENCENT Inc.All Rights Reserved.
+ *
+ * Description：
+ * 
+ * History：
+ * 
+ * User				Date			Info		Reason
+ * Gary		2014-10-31		Create		
+ * </pre>
+ */
+public class SampleActivity extends BaseActivity {
+
+	@Override
+	public void initLayout() {
+		setContentView(R.layout.sample_layout);
+	}
+}
+
